126ec673e1043e78ae5c004d73e30a39dd11567f
Abhishek kumar <54079908+abhishekak048@users.noreply.github.com>
2023-09-13 16:41:22 +0530

    helllo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect } from 'react';
+export default function IndexPage() {
+  const Barcode = "1234"
+  useEffect(() => {
+    console.log('heyyaaa');
+    try{
+    if (window.Barcode && window.Barcode.postMessage) {
+      Barcode.postMessage("hello bro hi");
+   }
+  } catch(e){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  });
+  return <div>Hello World.</div>;
 }
-
-export default App;
